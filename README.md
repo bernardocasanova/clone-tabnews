e5bb5162601136f94bfa1f75336fe921c78070dd
Bernardo Casanova <bernardocasanova@gmail.com>
2025-12-04 19:46:51 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementaçao do tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Parabens voce conseguiu fazer o amend!</h1>
+  return <h1>Parabens voce conseguiu fazer o amend!</h1>;
 }
 
 export default Home;
